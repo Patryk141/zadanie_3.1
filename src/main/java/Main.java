e13a7cc544a019e39d3dea594de941e42b157a7c
Patryk141 <115901692+Patryk141@users.noreply.github.com>
2022-10-16 11:17:12 +0200

    Revert "Branch#2"

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,13 +4,10 @@ public class Main {
     public static void main(String[] args) {
         Book book = new Book("Tolkien","The lord of the rings");
         Book book1 = new Book("J.K. Rowling","Harry Potter");
-        Book book2 = new Book("Stephen King","It");
         Gson gson = new Gson();
         String bookJson = gson.toJson(book);
         String book1Json = gson.toJson(book1);
-        String book2Json = gson.toJson(book2);
         System.out.println(bookJson);
         System.out.println(book1Json);
-        System.out.println(book2Json);
     }
 }
